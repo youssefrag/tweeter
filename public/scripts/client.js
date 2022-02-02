5c25d3ff0a6f9a4c50f72a5de1b62f78d903a304
Youssef Ragab <ragab.youssef95@gmail.com>
2022-02-02 16:57:14 -0500

    fetching tweets from /tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -33,30 +33,11 @@ $(() => {
   `);
   }
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+  $('#new-tweet-form').on('submit', (evt) => {
+    evt.preventDefault();
+    const param = $('#new-tweet-form').serialize()
+    $.post('/tweets', param)
+  })
 
   const renederTweets = function(tweets) {
     for (tweet of tweets) {
@@ -64,5 +45,11 @@ $(() => {
       $('#tweet-container').append($tweet);
     }
   }
-  renederTweets(data)
+
+  const loadTweets = function() {
+    $.ajax('/tweets', { method: 'GET'}).then((results) => {
+      renederTweets(results)
+    })
+  }
+  loadTweets();
 })

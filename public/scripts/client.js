154d7a4133c98cb1acf9df4c4225336d579dd9ee
Youssef Ragab <ragab.youssef95@gmail.com>
2022-02-02 15:37:27 -0500

    dynamic html added

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,30 +4,52 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready(function() {
-  const tweetFeedItem = document.getElementsByClassName('tweet-feed-item')
-  $(tweetFeedItem).on('mouseenter', function(){
-    $('.tweet-feed-item').addClass('shadow')
-  })
-  $(tweetFeedItem).on('mouseleave', function(){
-    $('.tweet-feed-item').removeClass('shadow')
-  })
-  $('#icon1').on('mouseenter', function(){
-    $('#icon1').addClass('icon-hover')
-  })
-  $('#icon1').on('mouseleave', function(){
-    $('#icon1').removeClass('icon-hover')
-  })
-  $('#icon2').on('mouseenter', function(){
-    $('#icon2').addClass('icon-hover')
-  })
-  $('#icon2').on('mouseleave', function(){
-    $('#icon2').removeClass('icon-hover')
-  })
-  $('#icon3').on('mouseenter', function(){
-    $('#icon3').addClass('icon-hover')
-  })
-  $('#icon3').on('mouseleave', function(){
-    $('#icon3').removeClass('icon-hover')
-  })
+import * as timeago from 'timeago.js';
+
+const createTweetElement =  (tweet) => {
+  return (`
+    <article class='tweet-feed-item'>
+      <div class='name-username'>
+          <div class='name-picture'>
+            <img
+              class='user-img'
+              src=${tweet.user.avatars}
+            />
+            <div class="name">${tweet.user.name}</div>
+          </div>
+          <div>${tweet.user.handle}</div>
+        </div>
+      <div class='tweet-content'>${tweet.content.text}</div>
+      <footer class='properties'>
+        <div class='time'>${tweet.created_at}</div>
+        <div class='icons'>
+          <i class="fas fa-flag icon"></i>
+          <i class="fas fa-retweet icon"></i>
+          <i class="fas fa-heart icon"></i>
+        </div>
+      </footer>
+    </article>
+  `);
+}
+
+const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+    "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+    "created_at": 1461116232227
+ }
+
+const $tweet = createTweetElement(tweetData)
+
+console.log(timeago.format(1461116232227))
+
+console.log($tweet)
+
+$(() => {
+  $('#tweet-container').append($tweet);
 })

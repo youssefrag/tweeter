e79f84cee669e00b682d1dc2d68641bf638413d4
Youssef Ragab <ragab.youssef95@gmail.com>
2022-02-03 20:37:49 -0500

    updated readme

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -14,77 +14,84 @@ const escape = function (str) {
 
 $(() => {
   const createTweetElement =  (tweet) => {
-  return (`
-    <article class='tweet-feed-item'>
-      <div class='name-username'>
-          <div class='name-picture'>
-            <img
-              class='user-img'
-              src=${tweet.user.avatars}
-            />
-            <div class="name">${tweet.user.name}</div>
+    return (`
+      <article class='tweet-feed-item'>
+        <div class='name-username'>
+            <div class='name-picture'>
+              <img
+                class='user-img'
+                src=${tweet.user.avatars}
+              />
+              <div class="name">${tweet.user.name}</div>
+            </div>
+            <div>${tweet.user.handle}</div>
           </div>
-          <div>${tweet.user.handle}</div>
-        </div>
-      <div class='tweet-content'>${escape(tweet.content.text)}</div>
-      <footer class='properties'>
-        <div class='time'>${timeago.format(tweet.created_at)}</div>
-        <div class='icons'>
-          <i class="fas fa-flag icon"></i>
-          <i class="fas fa-retweet icon"></i>
-          <i class="fas fa-heart icon"></i>
-        </div>
-      </footer>
-    </article>
-  `);
-  }
+        <div class='tweet-content'>${escape(tweet.content.text)}</div>
+        <footer class='properties'>
+          <div class='time'>${timeago.format(tweet.created_at)}</div>
+          <div class='icons'>
+            <i class="fas fa-flag icon"></i>
+            <i class="fas fa-retweet icon"></i>
+            <i class="fas fa-heart icon"></i>
+          </div>
+        </footer>
+      </article>
+    `);
+  };
 
   const emptyTweet = () => {
-    return(`
+    return (`
       <div class="error"><i class="error-icon fas fa-exclamation-triangle"></i>Cannot submit empty tweet.<i class="error-icon fas fa-exclamation-triangle"></i></div>
     `);
-  }
+  };
 
   const tooLongTweet = () => {
     return(`
-      <div class="error"><i class="error-icon fas fa-exclamation-triangle"></i>Tweet has exceeded character limit of 140.<i class="error-icon fas fa-exclamation-triangle"></i></div>
+      <div class="error"><i class="error-icon fas fa-exclamation-triangle"></i>Characted limit exceeded<i class="error-icon fas fa-exclamation-triangle"></i></div>
     `);
-  }
+  };
 
   $('#new-tweet-form').on('submit', (evt) => {
     evt.preventDefault();
-    const param = $('#new-tweet-form').serialize()
-    console.log(typeof $('.counter').val())
+    const param = $('#new-tweet-form').serialize();
+    // console.log(typeof $('.counter').val())
     if (Number($('.counter').val()) === 140) {
-      $('#error-messages').empty()
-      $('#error-messages').append(emptyTweet()).slideDown("slow")
+      $('#error-messages').empty();
+      $('#error-messages').append(emptyTweet()).slideDown("slow");
       
     } else if ($('.counter').val() < 0) {
-      $('#error-messages').empty()
-      $('#error-messages').append(tooLongTweet()).slideDown("slow")
-      // alert('Exceeded character limit')
+      // $('.counter').addClass('too-long')
+      $('#error-messages').empty();
+      $('#error-messages').append(tooLongTweet()).slideDown("slow");
     } else {
-      $('#error-messages').empty()
+      $('.counter').removeClass('too-long');
+      $('#error-messages').empty();
       $.post('/tweets', param).then(() => {
         $.ajax('/tweets', { method: 'GET'}).then((results) => {
-          const lastTweetAdded = results[results.length - 1]
-          const $lastTweetAdded = createTweetElement(lastTweetAdded)
+          // console.log(results);
+          const lastTweetAdded = results[results.length - 1];
+          // console.log(lastTweetAdded)
+          const $lastTweetAdded = createTweetElement;(lastTweetAdded)
+          console.log($lastTweetAdded)
           $('#tweet-container').append($lastTweetAdded);
-        })
-      })
+          event.stopPropagation()
+        });
+      });
     }
-  })
+    // $("#new-tweet-form").reset();
+    document.getElementById("new-tweet-form").reset();
+  });
 
   const renederTweets = function(tweets) {
     for (tweet of tweets) {
-      const $tweet = createTweetElement(tweet)
+      const $tweet = createTweetElement(tweet);
       $('#tweet-container').append($tweet);
     }
   }
 
   const loadTweets = function() {
     $.ajax('/tweets', { method: 'GET'}).then((results) => {
-      renederTweets(results)
+      renederTweets(results);
     })
   }
   loadTweets();
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
-  const newTweetForm = document.getElementById('new-tweet-form')
+  const newTweetForm = document.getElementById('new-tweet-form');
   newTweetForm.addEventListener('input', function() {
-    let length = $(this).find('#tweet-text').val().length
+    let length = $(this).find('#tweet-text').val().length;
     $(this).find('.btnAndCount').find('.counter').val(140 - length);
-  })
-})
+    if ($(".counter").val() < 0) {
+      $('.counter').addClass('too-long');
+    } else {
+      $('.counter').removeClass('too-long');
+    }
+  });
+});

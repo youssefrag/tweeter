e1fd20670180b9842f093c2a81031e21093add79
Youssef Ragab <ragab.youssef95@gmail.com>
2022-02-03 21:10:40 -0500

    final bug fixed

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -14,28 +14,28 @@ const escape = function (str) {
 
 $(() => {
   const createTweetElement =  (tweet) => {
-    return (`
-      <article class='tweet-feed-item'>
-        <div class='name-username'>
-            <div class='name-picture'>
-              <img
-                class='user-img'
-                src=${tweet.user.avatars}
-              />
-              <div class="name">${tweet.user.name}</div>
-            </div>
-            <div>${tweet.user.handle}</div>
+  return (`
+    <article class='tweet-feed-item'>
+      <div class='name-username'>
+          <div class='name-picture'>
+            <img
+              class='user-img'
+              src=${tweet.user.avatars}
+            />
+            <div class="name">${tweet.user.name}</div>
           </div>
-        <div class='tweet-content'>${escape(tweet.content.text)}</div>
-        <footer class='properties'>
-          <div class='time'>${timeago.format(tweet.created_at)}</div>
-          <div class='icons'>
-            <i class="fas fa-flag icon"></i>
-            <i class="fas fa-retweet icon"></i>
-            <i class="fas fa-heart icon"></i>
-          </div>
-        </footer>
-      </article>
+          <div>${tweet.user.handle}</div>
+        </div>
+      <div class='tweet-content'>${escape(tweet.content.text)}</div>
+      <footer class='properties'>
+        <div class='time'>${timeago.format(tweet.created_at)}</div>
+        <div class='icons'>
+          <i class="fas fa-flag icon"></i>
+          <i class="fas fa-retweet icon"></i>
+          <i class="fas fa-heart icon"></i>
+        </div>
+      </footer>
+    </article>
   `);
   };
 
@@ -63,25 +63,27 @@ $(() => {
       $('#error-messages').append(tooLongTweet()).slideDown("slow");
     } else {
       $('#error-messages').empty();
+      console.log('param', param)
       $.post('/tweets', param).then(() => {
+        document.getElementById("new-tweet-form").reset();
         $.ajax('/tweets', { method: 'GET'}).then((results) => {
-          // console.log(results);
+          console.log('results', results);
           const lastTweetAdded = results[results.length - 1];
           // console.log(lastTweetAdded)
-          const $lastTweetAdded = createTweetElement;(lastTweetAdded)
+          const $lastTweetAdded = createTweetElement(lastTweetAdded)
           console.log($lastTweetAdded)
-          $('#tweet-container').append($lastTweetAdded);
+          $('#tweet-container').prepend($lastTweetAdded);
         });
       });
     }
     // $("#new-tweet-form").reset();
-    document.getElementById("new-tweet-form").reset();
+    // document.getElementById("new-tweet-form").reset();
   });
 
   const renederTweets = function(tweets) {
     for (tweet of tweets) {
       const $tweet = createTweetElement(tweet);
-      $('#tweet-container').append($tweet);
+      $('#tweet-container').prepend($tweet);
     }
   }
 
